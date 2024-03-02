54e6f1a8da81c7f0d58ed2597822913be0c689a4
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-03-02 10:49:02 +0530

    readme file added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from dotenv import load_dotenv
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="ChatPDFs", page_icon=":books:")
     
     st.header("ChatPDFs :books:")
@@ -8,6 +11,8 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
 
 if __name__ == '__main__':
     main()
